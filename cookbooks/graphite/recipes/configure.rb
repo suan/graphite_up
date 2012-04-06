f4007b435f481495d8df119f611884ce54b537fa
Suan-Aik Yeo <syeo@enova.com>
2012-04-06 00:20:01 -0500

    finish configuration

--- cookbooks/graphite/recipes/configure.rb
+++ cookbooks/graphite/recipes/configure.rb
@@ -3,5 +3,29 @@ execute 'set-graphite-apache-config' do
   not_if "test -f /etc/apache2/sites-available/graphite"
 end
 
-# execute '' do
-# end
+execute 'create graphite.wsgi' do
+  command 'cp /opt/graphite/conf/graphite.wsgi.example /opt/graphite/conf/graphite.wsgi'
+  not_if 'test -f /opt/graphite/conf/graphite.wsgi'
+end
+
+execute 'create carbon.conf' do
+  command 'cp /opt/graphite/conf/carbon.conf.example /opt/graphite/conf/carbon.conf'
+  not_if 'test -f /opt/graphite/conf/carbon.conf'
+end
+
+execute 'create storage-schemas.conf' do
+  command 'cp /opt/graphite/conf/storage-schemas.conf.example /opt/graphite/conf/storage-schemas.conf'
+  not_if 'test -f /opt/graphite/conf/storage-schemas.conf'
+end
+
+execute 'setup Graphite django DB' do
+  command 'cd /opt/graphite/webapp/graphite && sudo python manage.py syncdb'
+end
+
+execute 'set graphite/storage directory perms' do
+  command 'chown -R www-data:www-data /opt/graphite/storage/'
+end
+
+execute 'use graphite vhost instead of default' do
+  command '/usr/sbin/a2dissite default && /usr/sbin/a2ensite graphite'
+end
